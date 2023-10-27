9184ea5cb75f74214169c9b873934a090d77855c
shahdhairya51 <shahdhairya51@gmail.com>
2023-10-28 03:25:52 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("Error connecting to mongodb");
+  });
 const app = express();
 
 app.listen(3000, () => {
