20303609c66abf35711f23f93bc78c541c516e52
shahdhairya51 <shahdhairya51@gmail.com>
2024-08-01 22:31:57 +0530

    complete image upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";  // Custom private route component to check authentication state.
 
 export default function App() {
     return (
@@ -15,7 +16,9 @@ export default function App() {
       <Route path = "/sign-in" element ={<SignIn />} />
       <Route path = "/sign-up" element ={<SignUp />} />
       <Route path = "/about" element ={<About />} />
+      <Route element = {<PrivateRoute />}>  {/* Only authenticated users can access this route */}
       <Route path = "/profile" element ={<Profile />} />
+      </Route>
     </Routes>
 
     </BrowserRouter>
